ceacb858cd4a618eb19c3dfd2300c2c2f2608004
Sandeep Kumar <sandeep6437@gmail.com>
2025-11-05 13:29:27 +0530

    Add files via upload

--- predictor_app.py
+++ predictor_app.py
@@ -1,3 +1,4 @@
+
 import plotly.graph_objects as go
 import streamlit as st
 import pandas as pd
@@ -5,26 +6,23 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
 # Load CSVs (assumed to be in the same directory)
-df_UTS = pd.read_csv("Dataframe_UTS.csv")
-df_FS = pd.read_csv("Dataframe_FS.csv")
-df_IS_Charpy = pd.read_csv("Dataframe_IS_Charpy.csv")
-df_IS_Izod = pd.read_csv("Dataframe_IS_Izod.csv")
+df_UTS = pd.read_csv("E:\My Research_Main\IIT Indore Conference\ML Code\Dataframe_UTS.csv")
+df_FS = pd.read_csv("E:\My Research_Main\IIT Indore Conference\ML Code\Dataframe_FS.csv")
+df_IS_Charpy = pd.read_csv("E:\My Research_Main\IIT Indore Conference\ML Code\Dataframe_IS_Charpy.csv")
+df_IS_Izod = pd.read_csv("E:\My Research_Main\IIT Indore Conference\ML Code\Dataframe_IS_Izod.csv")
 
 # Prediction function
-def predict_all_models(layer_thickness, curing_temp, curing_time,
-                       weight_gain_uts, weight_gain_fs, weight_gain_charpy, weight_gain_izod,
+def predict_all_models(layer_thickness, curing_temp, curing_time, weight_gain,
                        uts_before, fs_before, charpy_before, izod_before):
-
-    # Define model info and assign respective weight gains
     targets = {
-        "UTS After 72h (MPa)": ("UTS Before (MPa)", df_UTS, uts_before, weight_gain_uts),
-        "Flexural Strength After 72h (MPa)": ("Flexural Strength Before (MPa)", df_FS, fs_before, weight_gain_fs),
-        "Impact Strength (Charpy) After 72h (kJ/m2)": ("Impact Strength (Charpy) Before (kJ/m2)", df_IS_Charpy, charpy_before, weight_gain_charpy),
-        "Impact Strength (Izod) After 72h (kJ/m2)": ("Impact Strength (Izod) Before (kJ/m2)", df_IS_Izod, izod_before, weight_gain_izod)
+        "UTS After 72h (MPa)": ("UTS Before (MPa)", df_UTS, uts_before),
+        "Flexural Strength After 72h (MPa)": ("Flexural Strength Before (MPa)", df_FS, fs_before),
+        "Impact Strength (Charpy) After 72h (kJ/m2)": ("Impact Strength (Charpy) Before (kJ/m2)", df_IS_Charpy, charpy_before),
+        "Impact Strength (Izod) After 72h (kJ/m2)": ("Impact Strength (Izod) Before (kJ/m2)", df_IS_Izod, izod_before)
     }
 
     results = {}
-    for target_name, (before_col, df, before_value, weight_gain) in targets.items():
+    for target_name, (before_col, df, before_value) in targets.items():
         features = ['Layer Thickness (µm)', 'Curing Temperature (in deg. Celcius)',
                     'Curing Time (in Minutes)', 'Weight Gain (24h, %)', before_col]
         X = df[features]
@@ -37,24 +35,16 @@ def predict_all_models(layer_thickness, curing_temp, curing_time,
         sample = pd.DataFrame([[layer_thickness, curing_temp, curing_time, weight_gain, before_value]], columns=X.columns)
         pred = model.predict(sample)[0]
         results[target_name] = round(pred, 2)
-
     return results
 
-
 # Streamlit UI
-st.title("SLA 3D Printed Material Property Predictor for Grey Resin V5 Developed by Sandeep Kumar, PhD Scholar, Mechanical Engineering, IIT Ropar, Punjab - 140001")
+st.title("SLA 3D Printed Material Property Predictor for Grey Resin V5 Prepared by Sandeep Kumar, IIT Ropar, PhD (Click on '>' symbol on top left corner for entering your input values)")
 
 st.sidebar.header("Enter Sample Input Values")
 layer_thickness = st.sidebar.slider("Layer Thickness (µm)", 25, 100, 50, step=25)
 curing_temp = st.sidebar.slider("Curing Temperature (°C)", 30, 80, 60)
 curing_time = st.sidebar.slider("Curing Time (minutes)", 5, 30, 15)
-
-# Separate weight gain inputs
-st.sidebar.markdown("### Weight Gain (24h, %)")
-weight_gain_uts = st.sidebar.number_input("Weight Gain (UTS)", min_value=0.0, max_value=5.0, value=1.5, step=0.1)
-weight_gain_fs = st.sidebar.number_input("Weight Gain (Flexural)", min_value=0.0, max_value=5.0, value=1.5, step=0.1)
-weight_gain_charpy = st.sidebar.number_input("Weight Gain (Charpy)", min_value=0.0, max_value=5.0, value=1.5, step=0.1)
-weight_gain_izod = st.sidebar.number_input("Weight Gain (Izod)", min_value=0.0, max_value=5.0, value=1.5, step=0.1)
+weight_gain = st.sidebar.number_input("Weight Gain (%)", min_value=0.0, max_value=5.0, value=1.5, step=0.1)
 
 st.sidebar.markdown("### Mechanical Properties (Before)")
 uts_before = st.sidebar.number_input("UTS Before (MPa)", value=55)
@@ -64,8 +54,7 @@ izod_before = st.sidebar.number_input("Impact Strength (Izod) Before (kJ/m²)", 
 
 if st.sidebar.button("Predict Properties After 72h"):
     with st.spinner("Predicting..."):
-        results = predict_all_models(layer_thickness, curing_temp, curing_time,
-                                     weight_gain_uts, weight_gain_fs, weight_gain_charpy, weight_gain_izod,
+        results = predict_all_models(layer_thickness, curing_temp, curing_time, weight_gain,
                                      uts_before, fs_before, charpy_before, izod_before)
 
     st.success("Prediction complete!")
@@ -82,6 +71,7 @@ if st.sidebar.button("Predict Properties After 72h"):
         "Izod Impact (kJ/m²)": [izod_before, results["Impact Strength (Izod) After 72h (kJ/m2)"]]
     }
 
+    import plotly.graph_objects as go
     fig = go.Figure(data=[
         go.Bar(name='Before', x=list(comparison_data.keys()), y=[v[0] for v in comparison_data.values()]),
         go.Bar(name='After 72h', x=list(comparison_data.keys()), y=[v[1] for v in comparison_data.values()])
